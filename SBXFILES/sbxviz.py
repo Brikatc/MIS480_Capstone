9b49a766e169295fc8c47353ef48c893d1096597
baristakat <bcarey0803@gmail.com>
2022-04-10 11:35:55 -0400

    more updates

--- SBXFILES/sbxviz.py
+++ SBXFILES/sbxviz.py
@@ -6,5 +6,12 @@ conn = psycopg2.connect(database="sbx_test", user='postgres',
                         password='bkc!12278895', host='localhost', port='5432')
 sql = "SELECT * FROM waste_table"
 dat = pd.read_sql_query(sql, conn)
-dat.plot(kind="bar", x="year", y="total_waste")
+df = pd.DataFrame(dat, columns="year", "total_waste", "directops_discarded",
+                  "licensedstores_discarded", "customers_discarded",
+                  "packagingmaterial", "total_previous_year", "percent_change")
+print(df)
+dat.plot(kind="line", x="year", y="total_waste")
 plt.show()
+dat.plot(kind="line", x="year", y="percent_change")
+plt.show()
+print(dat.describe())
