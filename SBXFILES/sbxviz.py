20b9ad3221600b41843c17604ed7ca93ce596a26
baristakat <bcarey0803@gmail.com>
2022-04-10 13:56:34 -0400

    finished code

--- SBXFILES/sbxviz.py
+++ SBXFILES/sbxviz.py
@@ -1,16 +1,25 @@
 import pandas as pd
 import psycopg2
 import matplotlib.pyplot as plt
+from matplotlib import interactive
 
 conn = psycopg2.connect(database="sbx_test", user='postgres',
                         password='bkc!12278895', host='localhost', port='5432')
 sql = "SELECT * FROM waste_table"
 dat = pd.read_sql_query(sql, conn)
 df = pd.DataFrame(dat, columns=["year","total_waste","total_previous_year","percent_change"])
-print(df)
-dat.plot(kind="line", x="year", y="total_waste")
-plt.show()
-dat.plot(kind="line", x="year", y="percent_change")
-plt.show()
+print(df.head())
+p_line1=dat.plot(kind="line", x="year", y="total_waste")
+plt.figure(1)
+p_bar1=dat.plot(kind="bar", x="year", y="total_waste")
+plt.figure(2)
+p_line2=dat.plot(kind="line", x="year", y="percent_change")
+plt.figure(3)
+p_bar2=dat.plot(kind="bar", x="year", y="percent_change")
+plt.figure(4)
 print(df.describe())
 print(df.std())
+plt.show()
+
+
+
